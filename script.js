3f5a4abcc10af84a4b8ad39ffca187bae780a47f
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2021-08-23 20:28:29 +0530

    js

---
+++ script.js
@@ -0,0 +1,3 @@
+// // document.getElementById("a1").innerText = "I am changed"
+
+// document.getElementById("a3").innerHTML = ""
---
+++ storejsCode.js
@@ -0,0 +1,69 @@
+// a = 40
+// b = 20 
+// sum = a + b
+// diff =a - b
+// mul = a*b 
+// div = a/b
+// // console.log is used to print things - comments
+// console.log("The sum is",sum)
+// console.log("The diff is",diff)
+// console.log(mul)
+// // console.log(div)
+
+// // Division of 10/20 is 0.5
+// // console.log("Division of", a, "/",b,"is", div)
+
+// console.log(`Divison of ${a}/${b} is ${div}`)
+
+// s1 = "hello"
+// s2 = "world"
+// console.log(s1+s2)
+
+
+//let and const
+
+// let a = 10
+// const b = 20
+// a = 100
+
+// student record
+//  const name = "Abhishek"
+//  const id = "573"
+//  let sec = "I"
+//  let year = "1st"
+
+
+// let sum =  10+3 => operators
+
+// +, -, *,/, % => Airthmetic operators
+
+
+// conditional opeartor
+//  > >= < <= == !=
+// a = 10
+// b = 10 
+// console.log(a == b)  // true
+
+//a = 10 // => value of a is equal to 10
+//a == 10 // check id the value of a is equal to 10 or not
+
+//if else 
+// check id a number is positive or negative
+
+a =  0 // => value of a is
+// if a > 0  than a is positive
+if(a>0){
+    console.log("Its a positive")
+}
+else if(a<0){
+    console.log("Its a negative number")
+}
+else{
+    console.log("Its a zero no doubt")
+}
+
+
+// else if 
+
+
+
